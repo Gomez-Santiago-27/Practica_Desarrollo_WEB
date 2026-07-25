5956f1d10898c16479726cfc6f75125f763507a9
Santiago Gomez <santiagodavidg13@gmail.com>
2026-07-25 11:40:54 -0300

    Avances Cambia-color 25_07_2026 the commit message for your changes. Lines starting  will be ignored, and an empty message aborts the files.

--- Entrenamiento/src/main.js
+++ Entrenamiento/src/main.js
@@ -2,9 +2,24 @@ const cajaColoreada = document.querySelector('#caja_coloreada');
 
 const estilosDeLaCajaColoreada = window.getComputedStyle(cajaColoreada);
 
-const boton = document.querySelector('#boton_principal');
+const cambiarColorSeleccionado = document.querySelector('#cambiar_color_seleccionado');
 
-const listaDeColores = ['#000000', '#0000FF', '#FF00FF', '#008000', '#00FF00', '#FF0000'];
+const cambiarColorAleatorio = document.querySelector('#cambiarColorAleatorio');
+
+const selectorDeColores = document.querySelector('#selector_de_colores');
+
+const listaDeColores = [
+  '#000000', // negro
+  '#ff0000', // rojo
+  '#00ff00', // verde
+  '#0000ff', // azul
+  '#00ffff', // cyan
+  '#ff00ff', // magenta
+  '#ffff00', // amarillo
+  '#ff8000', // naranja
+  '#008000', // verde oscuro
+  '#800080'  // violeta
+];
 
 /**
  * 
@@ -28,8 +43,21 @@ function obtenerColorAleatorio() {
  * hace click.
  */
 function cambiarColorDeLaCaja() {
-  cajaColoreada.style.backgroundColor = obtenerColorAleatorio();
-
+  let colorActual = estilosDeLaCajaColoreada.backgroundColor;
+  let nuevoColor = obtenerColorAleatorio();
+  while (nuevoColor === colorActual) {
+    nuevoColor = obtenerColorAleatorio();
+  }
+  cajaColoreada.style.backgroundColor = nuevoColor;
 }
 
-boton.addEventListener('click', cambiarColorDeLaCaja);
+/**
+ * post: cambia el color de la caja por el color seleccionado.
+ */
+function cambiarPorColorSeleccionado() {
+  let colorSeleccionado = listaDeColores[selectorDeColores.value];
+  cajaColoreada.style.backgroundColor = colorSeleccionado;
+}
+
+cambiarColorAleatorio.addEventListener('click', cambiarColorDeLaCaja);
+cambiarColorSeleccionado.addEventListener('click', cambiarPorColorSeleccionado);
